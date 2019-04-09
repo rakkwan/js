5e7001d34aced9ac516449f798a7dbb8b90a2265
Jittima Goodrich <jgoodrich4@mail.greenriver.edu>
2019-04-08 23:00:47 -0400

    Add js2.html and js2.js

---
+++ chapter2/js2.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        console.log("Hee Haw!");
+    }
+    else if (i % 5 == 0)
+    {
+        console.log("Haw!");
+    }
+    else if (i % 3 == 0)
+    {
+        console.log("Hee!");
+    }
+    else
+    {
+        console.log(i);
+    }
+}
